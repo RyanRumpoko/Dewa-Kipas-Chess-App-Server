644af3f6c2a86c9c3e93fc77a7b9aff7bb300dee
Ferdian Atmadiputra <74676517+ferdianatmadiputra@users.noreply.github.com>
2021-03-20 02:05:57 +0700

    socket-init

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const router = require("./routes/index");
 const errHandler = require("./middlewares/errorHandler");
 const app = express();
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(cors());
 app.use(express.json());
@@ -14,5 +16,50 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", router);
 
 app.use(errHandler);
+
+const rooms = []
+io.on('connection', (socket) => {
+  
+  console.log('new connection from', socket.id)
+  let playerId = socket.id
+
+  socket.on('message', function (msg) {
+    console.log('got message from client: ' + msg)
+  })
+
+  socket.on('disconnect', function () {
+    console.log(playerId + ' disconnected');
+  })
+
+  socket.on('joined', function (roomId) {
+    // if the room is not full then add the player to that room
+    if (games[roomId].players < 2) {
+        games[roomId].players++;
+        games[roomId].pid[games[roomId].players - 1] = playerId;
+    } // else emit the full event
+    else {
+        socket.emit('full', roomId)
+        return;
+    }
+    console.log(games[roomId]);
+    players = games[roomId].players
+    // the first player to join the room gets white
+    if (players % 2 == 0) color = 'black';
+    else color = 'white';
+
+    // this is an important event because, once this is emitted the game
+    // will be set up in the client side, and it'll display the chess board
+    socket.emit('player', {
+        playerId,
+        players,
+        color,
+        roomId
+    })
+
+  });
+
+
+
+})
 
 module.exports = app;
