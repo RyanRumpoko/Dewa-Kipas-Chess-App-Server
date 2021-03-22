3947233d8244bcbb6c3320adecf3c4e253f93961
RyanRumpokocd..clear <xiipa4ryanrumpoko@gmail.com>
2021-03-22 23:48:36 +0700

    Update Access Token & GameOver

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const { User } = require("../models");
 const { comparePassword } = require("../helpers/bcrypt");
-const generateToken = require('../helpers/jwt')
+const generateToken = require("../helpers/jwt");
 
 class UserController {
   static register(req, res, next) {
@@ -43,66 +43,71 @@ class UserController {
           pictureUrl: user.pictureUrl,
           eloRating: user.eloRating,
         });
-        res
-          .status(200)
-          .json({
+        res.status(200).json({
+          access_token: access_token,
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          pictureUrl: user.pictureUrl,
+          eloRating: user.eloRating,
+        });
+      })
+      .catch((err) => next(err));
+  }
+  static getLeaderboard(req, res, next) {
+    const { testError } = req.body;
+    if (testError) throw { name: "INVALID_DATA", message: "TEST ERROR" };
+    User.findAll()
+      .then((users) => res.status(200).json(users))
+      .catch((err) => {
+        next(err);
+      });
+  }
+  static googleLogin(req, res, next) {
+    const { googleId, imageUrl, email, name } = req.body;
+    User.findOne({
+      where: {
+        email,
+      },
+    })
+      .then((user) => {
+        if (user) {
+          const access_token = generateToken({
             id: user.id,
             username: user.username,
             email: user.email,
             pictureUrl: user.pictureUrl,
             eloRating: user.eloRating,
           });
+          console.log(access_token, "?????????????????????");
+          res
+            .status(200)
+            .json({ username: user.username, email: user.email, access_token });
+        } else {
+          return User.create({
+            username: name,
+            email,
+            password: "defaultPassword",
+          }).then((registeredUser) => {
+            console.log(registeredUser, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            const access_token = generateToken({
+              id: registeredUser.id,
+              username: registeredUser.username,
+              email: registeredUser.email,
+              pictureUrl: registeredUser.pictureUrl,
+              eloRating: registeredUser.eloRating,
+            });
+            res
+              .status(201)
+              .json({
+                username: registeredUser.username,
+                email: registeredUser.email,
+                access_token,
+              });
+          });
+        }
       })
       .catch((err) => next(err));
-  }
-  static getLeaderboard(req, res, next) {
-    const { testError } = req.body
-    if(testError) throw { name: "INVALID_DATA", message: "TEST ERROR"  }
-    User.findAll()
-      .then((users) => res.status(200).json(users))
-      .catch((err) => {
-        next(err)
-      });
-  }
-  static googleLogin(req, res, next) {
-    const { googleId, imageUrl, email, name } = req.body
-    User.findOne({
-      where: {
-        email
-      }
-    }).then(user => {
-      if (user) {
-        const access_token = generateToken({
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          pictureUrl: user.pictureUrl,
-          eloRating: user.eloRating,
-        })
-        console.log(access_token, "?????????????????????");
-        res
-        .status(200)
-        .json({ username: user.username, email: user.email, access_token });
-      } else {
-        return User.create({
-          username: name,
-          email,
-          password: "defaultPassword",
-        }).then(registeredUser => {
-          console.log(registeredUser, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-          const access_token = generateToken({
-            id: registeredUser.id,
-            username: registeredUser.username,
-            email: registeredUser.email,
-            pictureUrl: registeredUser.pictureUrl,
-            eloRating: registeredUser.eloRating,
-          })
-          res
-            .status(201)
-            .json({ username: registeredUser.username, email: registeredUser.email, access_token });
-        })
-      }
-    }).catch(err => next(err))
   }
 }
 
